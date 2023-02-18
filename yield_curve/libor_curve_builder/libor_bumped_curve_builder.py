b7041a768e02299cd202f33d275de649b4d41a1e
CodeWithLuke <79409626+CodeWithLuke@users.noreply.github.com>
2023-02-18 16:02:13 -0500

    added pv01 to swap

--- yield_curve/libor_curve_builder/libor_bumped_curve_builder.py
+++ yield_curve/libor_curve_builder/libor_bumped_curve_builder.py
@@ -4,8 +4,26 @@ from utils.constants import BASIS_POINT_CONVERSION
 
 from copy import deepcopy
 
+from typing import Dict
 
-def bump_libor_curve(libor_curve: LiborCurve, n_bps_bump=1):
+
+def bump_all_libor_curve_nodes(libor_curve: LiborCurve, n_bps_bump=1) -> LiborCurve:
+    market_data = libor_curve.market_quotes
+
+    market_data_copy = deepcopy(market_data)
+
+    for curve_instrument, instrument_quotes in market_data.items():
+
+        for time, quote in instrument_quotes.items():
+
+            new_quote = quote + n_bps_bump * BASIS_POINT_CONVERSION
+
+            market_data_copy[curve_instrument][time] = new_quote
+
+    return LiborCurveBuilder.from_market_data_dict(market_data_copy).curve()
+
+
+def bump_libor_curve_by_node(libor_curve: LiborCurve, n_bps_bump=1) -> Dict[str, LiborCurve]:
 
     bumped_curves = dict()
 
