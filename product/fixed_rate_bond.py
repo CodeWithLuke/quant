4d8d5e5008e540dbb037d577f2e613ec4a47e7f9
CodeWithLuke <79409626+CodeWithLuke@users.noreply.github.com>
2023-03-16 18:42:44 -0400

    added z spread and ir curve valuation to fixed rate bond

--- product/fixed_rate_bond.py
+++ product/fixed_rate_bond.py
@@ -3,6 +3,7 @@ from math import exp
 from scipy.optimize import newton
 
 from utils.enum import CashFlowFrequency
+from yield_curve.libor_curve import LiborCurve
 
 
 class FixedRateBond:
@@ -17,6 +18,20 @@ class FixedRateBond:
         self.coupon_period = 1 / int(coupon_frequency)
         self.coupon_rate = coupon_rate
         self.num_coupon_payments = int(int(coupon_frequency) * maturity)
+
+    def present_value(self, libor_curve, applied_z_spread=0):
+        coupon_amount = self.notional * self.coupon_rate * self.coupon_period
+
+        value = self.notional * exp(-1 * self.maturity * (libor_curve.interpolate_curve(self.maturity) + applied_z_spread))
+
+        for i in range(1, self.num_coupon_payments + 1):
+            t = i * self.coupon_period
+
+            ir_yield = libor_curve.interpolate_curve(t)
+
+            value += coupon_amount * self.coupon_period * exp(-1 * t * (ir_yield + applied_z_spread))
+
+        return value
 
     def yield_to_price(self, yield_rate: float):
         coupon_amount = self.notional * self.coupon_rate * self.coupon_period
@@ -51,3 +66,11 @@ class FixedRateBond:
         return coupon_time_weighted_present_value_sum + self.maturity * self.notional * exp(
             -1 * self.maturity * yield_rate
         )
+
+    def get_z_spread(self, price: float, libor_curve: LiborCurve):
+
+        def _f(z): return self.present_value(libor_curve, z) - price
+
+        return newton(_f, 0)
+
+
