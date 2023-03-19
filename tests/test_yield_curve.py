8826f6e149000ce720935b828212568d3b65e944
CodeWithLuke <79409626+CodeWithLuke@users.noreply.github.com>
2023-03-19 17:40:25 -0400

    added discount factor test

--- tests/test_yield_curve.py
+++ tests/test_yield_curve.py
@@ -24,6 +24,17 @@ def test_yield_curve_construction():
 
     for i, expected_rate in enumerate(expected_rates):
         expected_rate_scaled = expected_rate / 100
-        error_msg =  f"Failed at index {i}. {curve._s[i]} and {expected_rate}"
-        assert curve._s[i] == pytest.approx(expected_rate_scaled, abs=UNIT_TEST_ABS_TOLERANCE,
-                                            rel=UNIT_TEST_REL_TOLERANCE), error_msg
+        assert curve._s[i] == pytest.approx(expected_rate_scaled, abs=UNIT_TEST_ABS_TOLERANCE, rel=UNIT_TEST_REL_TOLERANCE)
+
+def test_discount_factor_calc():
+    deposits = {1 / 52: 2.0, 1 / 12: 2.2, 1 / 6: 2.27, 1 / 4: 2.36}
+    futures = {6 / 12: 97.4, 9 / 12: 97.0}
+    swap_rate = {1.0: 3.0, 2.0: 3.6, 3.0: 3.95, 4.0: 4.2}
+    fcb = LiborCurveBuilder(deposits, futures, swap_rate)
+    curve = fcb.curve()
+
+    df_1 = curve.interpolate_discount_factor(3.0)
+    df_2 = curve.interpolate_discount_factor(3.5)
+    v = 100000 * (df_2 * 0.5 * 0.02 + df_2 - df_1)
+
+    assert v == pytest.approx(-1254.8198358152129, abs=UNIT_TEST_ABS_TOLERANCE, rel=UNIT_TEST_REL_TOLERANCE)
