7e83729b6fc4c0bf7922f2b60dc8385fd0d22816
CodeWithLuke <79409626+CodeWithLuke@users.noreply.github.com>
2023-03-19 17:33:36 -0400

    added yield curve test

---
+++ tests/test_yield_curve.py
@@ -0,0 +1,29 @@
+from yield_curve.libor_curve_builder.libor_curve_builder import LiborCurveBuilder
+
+from utils.constants import *
+
+import pytest
+
+def test_yield_curve_construction():
+    deposits = {1 / 52: 2.0, 1 / 12: 2.2, 1 / 6: 2.27, 1 / 4: 2.36}
+    futures = {6 / 12: 97.4, 9 / 12: 97.0}
+    swap_rate = {1.0: 3.0, 2.0: 3.6, 3.0: 3.95, 4.0: 4.2}
+    fcb = LiborCurveBuilder(deposits, futures, swap_rate)
+    curve = fcb.curve()
+
+    expected_rates = [1.9996154832063695,
+                       2.197985794764072,
+                       2.265716716659804,
+                       2.3530652632622195,
+                       2.4723258511729345,
+                       2.644485879275427,
+                       2.9815170310766788,
+                       3.5823684733174996,
+                       3.936610028120395,
+                       4.193130458902937]
+
+    for i, expected_rate in enumerate(expected_rates):
+        expected_rate_scaled = expected_rate / 100
+        error_msg =  f"Failed at index {i}. {curve._s[i]} and {expected_rate}"
+        assert curve._s[i] == pytest.approx(expected_rate_scaled, abs=UNIT_TEST_ABS_TOLERANCE,
+                                            rel=UNIT_TEST_REL_TOLERANCE), error_msg
