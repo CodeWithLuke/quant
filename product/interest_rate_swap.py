beefa11ee7861bb1111a41c719bb57aa1a53d79d
CodeWithLuke <79409626+CodeWithLuke@users.noreply.github.com>
2023-03-26 20:09:24 -0400

    moved swap_leg_cash_flow class into IR Swap

--- product/interest_rate_swap.py
+++ product/interest_rate_swap.py
@@ -1,9 +1,8 @@
 from typing import List
 
-from product.swap_leg_cash_flow import SwapLegCashFlow
 from utils.enum import CashFlowFrequency, InterestType, PayerReceiver, SwapLegType
 from yield_curve.abs_curve import AbsCurve
-
+from dataclasses import dataclass
 
 class InterestRateSwap:
 
@@ -180,3 +179,26 @@ class InterestRateSwap:
     @property
     def payer_receiver(self):
         return self._payer_receiver
+
+@dataclass
+class SwapLegCashFlow:
+    time: float
+    projected_rate: float
+    leg_type: SwapLegType
+    notional: float
+    discount_factor: float
+    cash_flow_frequency: CashFlowFrequency
+
+    def get_cash_flow_present_value(self):
+        return self.projected_rate * self.notional * self.discount_factor / float(self.cash_flow_frequency)
+
+    def get_cash_flow_future_value(self):
+        return self.projected_rate * self.notional / float(self.cash_flow_frequency)
+
+    def as_dict(self):
+        d = self.__dict__
+
+        d.update({
+            "present_value": self.get_cash_flow_present_value(), "future_value": self.get_cash_flow_future_value()
+        })
+        return d
