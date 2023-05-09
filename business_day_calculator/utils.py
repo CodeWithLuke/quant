a803428816334ec291f8dd64f813a0733eac9e44
CodeWithLuke <79409626+CodeWithLuke@users.noreply.github.com>
2023-05-09 18:17:54 -0400

    added annual day count fraction formula

--- business_day_calculator/utils.py
+++ business_day_calculator/utils.py
@@ -1,8 +1,10 @@
+import datetime
 from datetime import date, timedelta
 from holidays import financial_holidays, country_holidays
 from utils.constants import WEEKEND_INDICES
 from utils.enum import DayCountConvention
 from functools import cache
+from calendar import isleap
 
 
 def get_num_business_days (end_date: date, base_date: date = date.today(), region: str = 'NYSE'):
@@ -26,6 +28,9 @@ def get_holiday_calendar (region):
 
     return loaded_holidays
 
+def is_leap_year (year: int):
+    return isleap(year)
+
 
 def _date_range(base_date: date, end_date:date):
     for n in range(int((end_date - base_date).days)):
@@ -34,7 +39,8 @@ def _date_range(base_date: date, end_date:date):
 def is_business_day (base_date: date, loaded_holidays):
     return not (base_date.weekday() in WEEKEND_INDICES or base_date in loaded_holidays)
 
-def annual_day_count_fraction (base_date: date, end_date:date, day_count_convention: DayCountConvention, region: str = 'NYSE'):
+
+def annual_day_count_fraction (base_date: date, end_date:date, day_count_convention: DayCountConvention):
     if base_date > end_date:
         raise ValueError("end_date must be later than base_date")
 
@@ -51,6 +57,32 @@ def annual_day_count_fraction (base_date: date, end_date:date, day_count_convent
     else:
         actual_d = (end_date - base_date).days
 
-        if day_count_convention == DayCountConvention.DCC_ACTUAL_ACTUAL:
+        if day_count_convention == DayCountConvention.DCC_ACTUAL_365:
 
-            days_per_year = ba
+            return actual_d / 365
+
+        elif day_count_convention == DayCountConvention.DCC_ACTUAL_ACTUAL:
+
+            if base_date.year == end_date.year:
+                if is_leap_year(base_date.year):
+                    return (end_date - base_date).days / 366
+                else:
+                    return (end_date - base_date).days / 365
+
+
+            are_leap_years = [is_leap_year(y) for y in range(base_date.year, end_date.year + 1)]
+
+            # simple case where there is no leap year
+            if not any(are_leap_years):
+                return actual_d / 365
+
+            # leap year is in the middle, don't have to work with fractional leap year
+            elif not any((are_leap_years[0], are_leap_years[-1])):
+                actual_d -= sum(are_leap_years)
+                return actual_d / 365
+
+            else:
+                # TODO: implement the leap year case
+                raise NotImplementedError
+
+
